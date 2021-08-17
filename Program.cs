b55fea95d45252d984a04d60196d47395699eee4
@lucas.felipe <l17.lucas@gmail.com>
2021-08-17 12:34:44 -0300

    usando expr regulares, falta tostring, object e equals

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ namespace Projeto_string_exp_regulares
         {
             string url = "http://exemplo.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
 
-            //int interrog = url.IndexOf('?')+1;
-              
-            //url = url.Substring(interrog);
-              
+            int interrog = url.IndexOf('?')+1;
+
+            url = url.Substring(interrog);
+            string testando1 = "teste";
+            int teste2 = 18;
             ExtraiArgumentos a = new ExtraiArgumentos(url);
               
             //Console.WriteLine(a.GetValor("moedaOrigem").Remove(a.GetValor("moedaOrigem").IndexOf('&')));
@@ -22,6 +23,9 @@ namespace Projeto_string_exp_regulares
             //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
             string padrao = "[0-9]{4,5}-?[0-9]{4}";
             Console.WriteLine(Regex.Match(teste,padrao).Value);
+
+            Console.WriteLine($"url: {a.GetValor("moedaOrigem")}, {testando1}, {teste2*6+1}");
+
 
         }
     }
